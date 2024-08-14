2657307b737e65d7f7732cb65993490ffd4e1325
Suanynet <sarrieta@fwdcostarica.com>
2024-08-14 09:07:14 -0600

    Finalización del ejercicio 2

--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,7 +1,12 @@
 class Contador {
-  // Inserte el código aquí
+
+  valor = 0
+  siguiente() {
+    return this.valor++
+  }
+  
 }
 
-// Inserte el código aquí
+const instancia = new Contador
 
 export { instancia }
