ce1e256febc34da70f1187c62ba0130a660ac9fa
Suanynet <sarrieta@fwdcostarica.com>
2024-08-16 11:50:19 -0600

    Finalización del ejercicio3

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,5 +1,45 @@
-// Inserte el código aquí
+function clonarArreglo() {
+   
+    const newclon = [...newarreglo]
 
+
+    return newclon
+}
+
+const newarreglo = [1, 2, 3]
+
+clonarArreglo(newarreglo)
+
+
+
+function clonarObjeto() {
+
+    const objeto = {nombre: "Maria", edad: 20}
+    
+    const result = {...objeto}
+
+    return result
+    
+}
+
+function sumar(...numeros1) {
+
+    return numeros1.reduce((acumulador, numerosActual) => {
+        return acumulador + numerosActual
+    }, 0)
+    
+    
+}
+
+sumar(10,20,30,40,50)
+
+
+
+function saludar(nombre = `Desconocido`) {
+
+    return "Hola", nombre
+
+}
 
 
 // NO MODIFICAR
